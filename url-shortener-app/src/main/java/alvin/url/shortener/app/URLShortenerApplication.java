b8e04754adf0514759af3e88039b2825084afa0c
alvin chandra <alvinchandra99@gmail.com>
2022-01-20 21:41:23 +0700

    init maven multi module project

---
+++ url-shortener-app/src/main/java/alvin/url/shortener/app/URLShortenerApplication.java
@@ -0,0 +1,11 @@
+package alvin.url.shortener.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class URLShortenerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(URLShortenerApplication.class, args);
+    }
+}
